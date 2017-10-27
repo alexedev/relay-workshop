b864998ca7bdd26a15f76a07aa287ee9eec4ee79
Alex Alexeev <alex.alexeev@kiwi.com>
2017-10-27 10:58:00 +0200

    query-renderer

---
+++ src/components/__generated__/AppContainerQuery.graphql.js
@@ -0,0 +1,277 @@
+/**
+ * @flow
+ * @relayHash 9b97c33b979e4b66c737aa84e09f1609
+ */
+
+/* eslint-disable */
+
+'use strict';
+
+/*::
+import type {ConcreteBatch} from 'relay-runtime';
+export type AppContainerQueryResponse = {|
+  +viewer: {|
+    +id: string;
+    +allCats: {|
+      +edges: ?$ReadOnlyArray<?{|
+        +cursor: string;
+        +node: {|
+          +id: string;
+          +fullName: ?string;
+          +nickname: string;
+          +imageUrl: string;
+          +isShwifty: ?boolean;
+        |};
+      |}>;
+    |};
+  |};
+|};
+*/
+
+
+/*
+query AppContainerQuery {
+  viewer {
+    id
+    allCats(last: 3) {
+      edges {
+        cursor
+        node {
+          id
+          fullName
+          nickname
+          imageUrl
+          isShwifty
+        }
+      }
+    }
+  }
+}
+*/
+
+const batch /*: ConcreteBatch*/ = {
+  "fragment": {
+    "argumentDefinitions": [],
+    "kind": "Fragment",
+    "metadata": null,
+    "name": "AppContainerQuery",
+    "selections": [
+      {
+        "kind": "LinkedField",
+        "alias": null,
+        "args": null,
+        "concreteType": "Viewer",
+        "name": "viewer",
+        "plural": false,
+        "selections": [
+          {
+            "kind": "ScalarField",
+            "alias": null,
+            "args": null,
+            "name": "id",
+            "storageKey": null
+          },
+          {
+            "kind": "LinkedField",
+            "alias": null,
+            "args": [
+              {
+                "kind": "Literal",
+                "name": "last",
+                "value": 3,
+                "type": "Int"
+              }
+            ],
+            "concreteType": "CatConnection",
+            "name": "allCats",
+            "plural": false,
+            "selections": [
+              {
+                "kind": "LinkedField",
+                "alias": null,
+                "args": null,
+                "concreteType": "CatEdge",
+                "name": "edges",
+                "plural": true,
+                "selections": [
+                  {
+                    "kind": "ScalarField",
+                    "alias": null,
+                    "args": null,
+                    "name": "cursor",
+                    "storageKey": null
+                  },
+                  {
+                    "kind": "LinkedField",
+                    "alias": null,
+                    "args": null,
+                    "concreteType": "Cat",
+                    "name": "node",
+                    "plural": false,
+                    "selections": [
+                      {
+                        "kind": "ScalarField",
+                        "alias": null,
+                        "args": null,
+                        "name": "id",
+                        "storageKey": null
+                      },
+                      {
+                        "kind": "ScalarField",
+                        "alias": null,
+                        "args": null,
+                        "name": "fullName",
+                        "storageKey": null
+                      },
+                      {
+                        "kind": "ScalarField",
+                        "alias": null,
+                        "args": null,
+                        "name": "nickname",
+                        "storageKey": null
+                      },
+                      {
+                        "kind": "ScalarField",
+                        "alias": null,
+                        "args": null,
+                        "name": "imageUrl",
+                        "storageKey": null
+                      },
+                      {
+                        "kind": "ScalarField",
+                        "alias": null,
+                        "args": null,
+                        "name": "isShwifty",
+                        "storageKey": null
+                      }
+                    ],
+                    "storageKey": null
+                  }
+                ],
+                "storageKey": null
+              }
+            ],
+            "storageKey": "allCats{\"last\":3}"
+          }
+        ],
+        "storageKey": null
+      }
+    ],
+    "type": "Query"
+  },
+  "id": null,
+  "kind": "Batch",
+  "metadata": {},
+  "name": "AppContainerQuery",
+  "query": {
+    "argumentDefinitions": [],
+    "kind": "Root",
+    "name": "AppContainerQuery",
+    "operation": "query",
+    "selections": [
+      {
+        "kind": "LinkedField",
+        "alias": null,
+        "args": null,
+        "concreteType": "Viewer",
+        "name": "viewer",
+        "plural": false,
+        "selections": [
+          {
+            "kind": "ScalarField",
+            "alias": null,
+            "args": null,
+            "name": "id",
+            "storageKey": null
+          },
+          {
+            "kind": "LinkedField",
+            "alias": null,
+            "args": [
+              {
+                "kind": "Literal",
+                "name": "last",
+                "value": 3,
+                "type": "Int"
+              }
+            ],
+            "concreteType": "CatConnection",
+            "name": "allCats",
+            "plural": false,
+            "selections": [
+              {
+                "kind": "LinkedField",
+                "alias": null,
+                "args": null,
+                "concreteType": "CatEdge",
+                "name": "edges",
+                "plural": true,
+                "selections": [
+                  {
+                    "kind": "ScalarField",
+                    "alias": null,
+                    "args": null,
+                    "name": "cursor",
+                    "storageKey": null
+                  },
+                  {
+                    "kind": "LinkedField",
+                    "alias": null,
+                    "args": null,
+                    "concreteType": "Cat",
+                    "name": "node",
+                    "plural": false,
+                    "selections": [
+                      {
+                        "kind": "ScalarField",
+                        "alias": null,
+                        "args": null,
+                        "name": "id",
+                        "storageKey": null
+                      },
+                      {
+                        "kind": "ScalarField",
+                        "alias": null,
+                        "args": null,
+                        "name": "fullName",
+                        "storageKey": null
+                      },
+                      {
+                        "kind": "ScalarField",
+                        "alias": null,
+                        "args": null,
+                        "name": "nickname",
+                        "storageKey": null
+                      },
+                      {
+                        "kind": "ScalarField",
+                        "alias": null,
+                        "args": null,
+                        "name": "imageUrl",
+                        "storageKey": null
+                      },
+                      {
+                        "kind": "ScalarField",
+                        "alias": null,
+                        "args": null,
+                        "name": "isShwifty",
+                        "storageKey": null
+                      }
+                    ],
+                    "storageKey": null
+                  }
+                ],
+                "storageKey": null
+              }
+            ],
+            "storageKey": "allCats{\"last\":3}"
+          }
+        ],
+        "storageKey": null
+      }
+    ]
+  },
+  "text": "query AppContainerQuery {\n  viewer {\n    id\n    allCats(last: 3) {\n      edges {\n        cursor\n        node {\n          id\n          fullName\n          nickname\n          imageUrl\n          isShwifty\n        }\n      }\n    }\n  }\n}\n"
+};
+
+module.exports = batch;
